060c763795ddca5d1c008ba6c15bc09ffcb4b263
PragyanKumar <81701254+PragyanKumar@users.noreply.github.com>
2021-07-25 20:59:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function new_image()
         block_image_object.scaleToWidth(700)
         block_image_object.scaleToHeight(510)
         block_image_object.set({
-            top:0
+            top:0,
             left:0
         });
         canvas.add(block_image_object)
